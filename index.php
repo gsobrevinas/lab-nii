21bf6aeeb2eecdbab76d7d9804dff841e295a327
Gian Sobrevinas <gsobrevinas@student.apc.edu.ph>
2024-04-02 12:40:49 +0800

    introduction

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE HTML>
+<head>
+<title>Site Internet de Fontaine</title>
+<link rel="icon" type="image/icon" href="./Images/logo.webp" >
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<style>
+body{
+	background-image: url("./Images/fontaine2.png");
+	background-repeat: no-repeat;
+	background-attachment: fixed;
+	background-size: cover;
+	margin:0;
+  padding:0;
+}
+
+.fontaine {
+  display: block;
+  margin-left:auto;
+  margin-right:auto;
+  border-radius: 8px;
+}
+
+.intro {
+	color:white;
+	text-align:center;
+}
+
+.stem {
+  text-align:center;
+  color:white;
+  font-family: 'palatino', serif;
+	font-size: 75px;
+}
+
+.nav ul {
+  list-style: none;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #2d4286;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+.nav li {
+  display: inline-block;
+}
+
+.nav li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 15px;
+  text-decoration: none;
+}
+
+.nav li a:hover {
+  background-color: #698ae8;
+  transition: 0.4s;
+}
+
+.logo{
+  display:block;
+  margin-right:auto;
+  margin-left:auto;
+  height: 45px;
+  width: 45px;
+}
+.ppp{
+	color:white;
+}
+</style>
+<body>
+<div class="nav">  
+<ul>
+  <li><a href="index.php">Home</a></li>
+  <li><a href="./News.php">News</a></li>
+  <li><img src="./Images/logo.webp" width="64px" height="64px" ></li>
+  <li><a href="./Archon.php">Artworks</a></li>
+  <li ><a href="./About.php">About</a></li>
+  <li><a href="./guestbook.php">Update Me!</a></li>
+</ul>
+</div>
+
+<div style="padding:20px;margin-top:30px;height:25px;">
+<h1 class ="stem">Welcome to Fontaine!</h1>
+<p class="intro">This is a website dedicated to showcase the Nation of Fontaine.</p>
+</div>
+
+<div class="ppp">
+<?php
+echo phpversion();
+?>
+</div>
+
+</body>
+</html>
